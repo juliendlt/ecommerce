51d2a10f406b6a0b1e4d7a826fe16dbcaffcecfd
julien.delmotte <julien.delmotte@ratp.fr>
2026-06-26 15:29:33 +0200

    [Final Backend]

---
+++ backend/prisma/migrations/20260626132724_init/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ProductImage` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "ProductImage" DROP CONSTRAINT "ProductImage_optionValueId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ProductImage" DROP CONSTRAINT "ProductImage_productId_fkey";
+
+-- DropTable
+DROP TABLE "ProductImage";
+
+-- CreateTable
+CREATE TABLE "product_images" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "alt" TEXT,
+    "position" INTEGER NOT NULL DEFAULT 0,
+    "optionValueId" TEXT,
+
+    CONSTRAINT "product_images_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "product_images" ADD CONSTRAINT "product_images_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_images" ADD CONSTRAINT "product_images_optionValueId_fkey" FOREIGN KEY ("optionValueId") REFERENCES "option_values"("id") ON DELETE SET NULL ON UPDATE CASCADE;
